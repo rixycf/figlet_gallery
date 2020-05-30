bc52e194781f66286219c54acc358c1697946a06
Riki Kaida <kasnake1013@gmail.com>
2020-05-30 21:34:52 +0900

    add model view update

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 
 
-main : Html msg
+type alias Model =
+    { input : String }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "", Cmd.none )
+
+
+type Msg
+    = Input String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input input ->
+            ( { model | input = input }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "hello" ]
+
+
+main : Program () Model Msg
 main =
-    text "hello"
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
